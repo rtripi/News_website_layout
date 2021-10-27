919eb2964cbbbeabd49798696be42211b9573b50
Renato Tripi <renatosft@gmail.com>
2021-10-27 17:35:13 -0300

    nav finished

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const nums = 3 && 2 && 1 && 0 && false;
-console.log(nums);
+const btnHam = document.querySelector(".ham-btn");
+const btnTimes = document.querySelector(".times-btn");
+const navBar = document.getElementById("nav-bar");
+
+btnHam.addEventListener("click", () => {
+  if (btnHam.classList !== "") {
+    btnHam.style.display = "none";
+    btnTimes.style.display = "block";
+    navBar.classList.add("show-nav");
+  }
+});
+
+btnTimes.addEventListener("click", () => {
+  if (btnHam.classList !== "") {
+    btnTimes.style.display = "none";
+    btnHam.style.display = "block";
+    navBar.classList.remove("show-nav");
+  }
+});
